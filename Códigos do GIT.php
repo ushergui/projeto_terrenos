3a4c3bbd6f85465ddfdb2e62da4d1d9e4e77c605
ushergui <guilherme-02-08@hotmail.com>
2018-11-28 16:53:24 -0200

    Signed-off-by: ushergui <guilherme-02-08@hotmail.com>

---
+++ Códigos do GIT.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Pra pegar um repositório
+git clone "url do repositório"
+
+Pra enviar mudanças
+git init (dentro da pasta que vc quer)  ---- Inicia os arquivos
+git add "nome do arquivo" ------------- Adiciona os arquivos
+git add --all ----------------------- Adiciona todos os arquivos
+git commit -n ok (se for mais de uma palavra, colocar entre aspas duplas - serve para descrever oq está fazendo) Enviar os arquivos 
+
+Pra evitar conflitos, faça primeiro um 
+
+git pull origin master e depois um push
+
+*/
+
+?>
